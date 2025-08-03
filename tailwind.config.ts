60dd783bc07cb3431dfcd71804b8dfc57581d73c
Daipayan Ghosal <daipayan.ghosal@gmail.com>
2025-08-03 02:33:21 +0000

    Nope use Noto Serif bengali for this

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ export default {
         body: ['Inter', 'sans-serif'],
         headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
-        bengali: ['Kalpurush', 'sans-serif'],
+        bengali: ['"Noto Serif Bengali"', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
