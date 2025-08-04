233f0099b33687bcdb3a683738c89c50462801d6
Daipayan Ghosal <daipayan.ghosal@gmail.com>
2025-08-04 05:59:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
-/** @type {import('next').NextConfig} */
-import withPWAInit from 'next-pwa';
+import withPWAInit from "next-pwa";
 
 const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  pwaExcludes: [
+    /.*firebase.*$/,
+  ],
 });
 
 const nextConfig = {
   reactStrictMode: true,
-  // Your other Next.js config options go here
 };
 
 export default withPWA(nextConfig);
